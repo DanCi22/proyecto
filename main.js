563aa48c0ea434ba89e9496f905831ac8ff498c1
DanCi22 <dla40026@gmail.com>
2024-09-18 22:15:43 -0600

    para seleccionar si la contraseña incluirá mayúsculas, minúsculas, números y símbolos, respectivamente.

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const form = document.getElementById('form');
 const password = document.getElementById('password');
 const copiar = document.getElementById('copiar');
 const numero = document.getElementById('numero');
+const mayus = document.getElementById('mayus');
+const minus = document.getElementById('minus');
+const numeros = document.getElementById('numeros');
+const simbolos = document.getElementById('simbolos');
