ed81fddb2edd36c80d0cc04c84281b6b0d4dbe84
DanCi22 <dla40026@gmail.com>
2024-09-18 22:17:44 -0600

    Agrega evento para copiar el password al portapapeles

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ const mayus = document.getElementById('mayus');
 const minus = document.getElementById('minus');
 const numeros = document.getElementById('numeros');
 const simbolos = document.getElementById('simbolos');
+
+copiar.addEventListener('click',()=>{
+
+    let pass = password.innerText;
+    if(!password){
+        return;
+    }
+
+    navigator.clipboard.writeText(pass);
+    alert('Password Copiado!');
+
+});
