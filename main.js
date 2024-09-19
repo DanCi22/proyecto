6f2ced49bfbfbee1fcc377586d2affdb8948529b
DanCi22 <dla40026@gmail.com>
2024-09-18 22:14:29 -0600

    campo donde se elige el número de caracteres de la contraseña.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 const form = document.getElementById('form');
 const password = document.getElementById('password');
 const copiar = document.getElementById('copiar');
+const numero = document.getElementById('numero');
